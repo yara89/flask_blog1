c73bd5f0ae6ed6dbec3a710359c7e56b5686345a
yara89 <60362934+yara89@users.noreply.github.com>
2021-02-15 16:27:00 +0100

    Update forms.py

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,35 +1,63 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
+
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-                        validators=[DataRequired(), Length(min=2, max=20)])
+                           validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', 
-                        validators=[DataRequired()])
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',
-                        validators=[DataRequired(), EqualTo('password')])
+                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
-        if user: 
-            raise ValidationError('the username is already taken, choose another one! ')
+        if user:
+            raise ValidationError(
+                'the username is already taken, choose another one! ')
 
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
-        if user: 
-            raise ValidationError('this email already has an account, sign up using a differnt email! ')
-
+        if user:
+            raise ValidationError(
+                'this email already has an account, sign up using a differnt email! ')
 
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField('Password', 
-                        validators=[DataRequired()])
+    password = PasswordField('Password',
+                             validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[
+                        FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update Profle')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError(
+                    'the username is already taken, choose another one! ')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError(
+                    'this email address already has an account, sign up using a differnt email! ')
